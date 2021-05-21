8d87e26328c219ff467d5c9e70026ac7dbd2745a
Bùi Công Lợi <hts.congloi@gmail.com>
2021-05-21 09:11:12 +0700

    update axios services

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -1,70 +1,34 @@
 import axios from 'axios'
-import Auth from '../helper/auth'
-class AxiosService {
-  constructor () {
-    axios.defaults.headers.common.Accept = 'application/json'
-    axios.defaults.headers.common['Content-Type'] = 'application/json'
-    // axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
-    const isLogined = Auth.isLogined()
-    if (isLogined) {
-      const userData = Auth.getUser()
-      if (userData.access_token) {
-        axios.defaults.headers.common.Authorization = `${userData.type} ${userData.access_token}`
-      }
+export default class AxiosService {
+  constructor (status) {
+    this.isAuth = status && status.auth ? status.auth : false
+    this.instance = axios.create({
+      baseURL: API_URL
+    })
+    return this.init()
+  }
+
+  init () {
+    if (this.isAuth) {
+      this.instance.interceptors.request.use(request => {
+        request.headers.authorization = AuthService.getBearer()
+        // if access token expired and refreshToken is exist >> go to API and get new access token
+        if (AuthService.isAccessTokenExpired() && AuthService.hasRefreshToken()) {
+          return AuthService.debounceRefreshTokens()
+            .then(response => {
+              AuthService.setBearer(response.data.accessToken)
+              request.headers.authorization = AuthService.getBearer()
+              return request
+            }).catch(error => Promise.reject(error))
+        } else {
+          return request
+        }
+      }, error => {
+        return Promise.reject(error)
+      })
     }
 
-    const instance = axios.create({
-      baseURL: process.env.VUE_APP_API_URL
-    })
-    instance.interceptors.response.use(this.handleSuccess, this.handleError)
-    this.instance = instance
-  }
-
-  handleSuccess (response) {
-    return response
-  }
-
-  handleError (error) {
-    if (error.response.status === 500) {
-      alert(
-        'Something went wrong !!!\nServer error with status code: ' +
-                error.response.status
-      )
-    }
-    return Promise.reject(error)
-  }
-
-  get (url) {
-    return this.checkValidToken(this.instance.get(url))
-  }
-
-  post (url, body) {
-    return this.checkValidToken(this.instance.post(url, body))
-  }
-
-  put (url, body) {
-    return this.checkValidToken(this.instance.put(url, body))
-  }
-
-  delete (url) {
-    return this.checkValidToken(this.instance.delete(url))
-  }
-
-  checkValidToken = data => {
-    const promise = new Promise(resolve => {
-      resolve(data)
-    })
-    promise.then(value => {
-      if (value.data.code === 500) {
-        alert('Something went wrong')
-      } else if (value.data.code === 403) {
-        alert('Unauthorized. Please try login again.')
-        Auth.removeUser()
-        window.location.href = '/login'
-      }
-    })
-    return data
+    return this.instance
   }
 }
-export default new AxiosService()
